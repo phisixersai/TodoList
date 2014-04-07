866c6a9196ab6f8c120f430d3970012c46787e2c
phisixersai <phisixersai@gmail.com>
2014-04-07 10:15:33 +0800

    update materials

---
+++ public/javascripts/index.js
@@ -0,0 +1,12 @@
+(function(){
+
+// 插入 <ul> 之 <li> 樣板
+var tmpl = '<li><input type="text"><span></span></li>',
+    addButton = $('#add'),
+    connected = $('.connected'),      // 三個 <ul>
+    placeholder = $('#placeholder'),  // 三個 <ul> 的容器
+    mainUl = $('.main'),              // main <ul>
+    deleteUl = $('.delete'),          // delete <ul>
+    doneUl = $('.done');              // done <ul>
+
+}());
